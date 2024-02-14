90450dfa0028c52cbfd7d176f6ce51e533969f77
Ivan Molero <imolerod@gmail.com>
2024-02-14 11:20:34 -0500

    se instala el soporte para mapstruct, se crean clases de dominio (estan en ingles)

--- Java_Spring/market/src/main/java/com/imolerodev/market/persistence/ProductoRepository.java
+++ Java_Spring/market/src/main/java/com/imolerodev/market/persistence/ProductoRepository.java
@@ -2,10 +2,14 @@ package com.imolerodev.market.persistence;
 
 import com.imolerodev.market.persistence.crud.ProductoCrudRepository;
 import com.imolerodev.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+// la anotación @Repository indica que esta clase interactua con la base de datos
+// tambien puede usarse @Component, solo que esta anotación es muy genérica
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +23,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }

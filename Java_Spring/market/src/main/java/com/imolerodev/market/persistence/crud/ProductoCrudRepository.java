dca46184eccc74e5005e05e4b46edcc83a69f056
Ivan Molero <imolerod@gmail.com>
2024-02-14 08:46:46 -0500

    implementación de algunos query methods, ver ProductoCrudRepository y ProductoRepository

--- Java_Spring/market/src/main/java/com/imolerodev/market/persistence/crud/ProductoCrudRepository.java
+++ Java_Spring/market/src/main/java/com/imolerodev/market/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,19 @@
 package com.imolerodev.market.persistence.crud;
 
 import com.imolerodev.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    // ojo que también es posible usar un native query para poder obtener el metodo
+    // si se usa una native query no es necesario seguir la regla de nombrado
+    // de query methods
+//    @Query(value = "select * from productos where id_categoria = ?", nativeQuery = true)
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }

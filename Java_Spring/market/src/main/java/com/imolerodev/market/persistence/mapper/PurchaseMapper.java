2b237d2d8e415b9580d18354a46c636d6c7f77fb
Ivan Molero <imolerod@gmail.com>
2024-02-15 18:58:35 -0500

    implementados los mappers para Purchase y PurchaseItem

---
+++ Java_Spring/market/src/main/java/com/imolerodev/market/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,31 @@
+package com.imolerodev.market.persistence.mapper;
+
+import com.imolerodev.market.domain.Purchase;
+import com.imolerodev.market.persistence.entity.Compra;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PurchaseMapper {
+    @Mappings({
+            @Mapping(source = "idCompra", target = "purchaseId"),
+            @Mapping(source = "idCliente", target = "clienteId"),
+            @Mapping(source = "fecha", target = "date"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "productos", target = "items")
+    })
+    Purchase toPurchase(Compra compra);
+    List<Purchase> toPurchases(List<Compra> compras);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "cliente", ignore = true)
+    })
+    Compra toCompra(Purchase purchase);
+}

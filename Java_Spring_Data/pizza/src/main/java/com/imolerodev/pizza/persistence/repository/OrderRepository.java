fdf6e9196ba0173988ea2f3c2cabb99fde3cf803
Ivan Molero <imolerod@gmail.com>
2024-02-26 21:30:39 -0500

    uso de querys nativas, revisar OrderRepository

--- Java_Spring_Data/pizza/src/main/java/com/imolerodev/pizza/persistence/repository/OrderRepository.java
+++ Java_Spring_Data/pizza/src/main/java/com/imolerodev/pizza/persistence/repository/OrderRepository.java
@@ -1,12 +1,19 @@
 package com.imolerodev.pizza.persistence.repository;
 
 import com.imolerodev.pizza.persistence.entity.OrderEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
     List<OrderEntity> findAllByDateAfter(LocalDateTime fecha);
+
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "select * from pizza_order where id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
+
 }

18a84c2a165c61b45f38300a3a8e73f9cc2db8d7
Ivan Molero <imolerod@gmail.com>
2024-03-02 21:51:40 -0500

    implementación de auditoria opcion 1, con jsonignore, ver PizzaApplication para la configuracion inicial y PizzaEntity para la configuración particular

--- Java_Spring_Data/pizza/src/main/java/com/imolerodev/pizza/persistence/entity/PizzaEntity.java
+++ Java_Spring_Data/pizza/src/main/java/com/imolerodev/pizza/persistence/entity/PizzaEntity.java
@@ -1,11 +1,18 @@
 package com.imolerodev.pizza.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class PizzaEntity {
@@ -29,5 +36,10 @@ public class PizzaEntity {
     @Column(nullable = false)
     private Boolean available;
 
+    @CreatedDate
+    @JsonIgnore
+    private LocalDateTime createdDate;
 
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
 }

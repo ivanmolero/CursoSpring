3b7345b73482ef7ef405685564d44a6aafa842b8
Ivan Molero <imolerod@gmail.com>
2024-03-06 12:27:38 -0500

    configuración inicial, deshabilita la configuración de autenticación en la forma antigua y la actual, ver el archivo SecurityConfig.java

---
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.imolerodev.pizza.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .permitAll();
+        http.authorizeHttpRequests(auth -> {
+            auth
+                    .anyRequest()
+                    .permitAll();
+        });
+        return http.build();
+    }
+}

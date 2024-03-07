b012a69a2b7a20ede5576a625ff917d8b9fd5d1b
Ivan Molero <imolerod@gmail.com>
2024-03-07 09:33:09 -0500

    creada la entidad para la gestión de los usuarios, falta enlazarlo a spring security

---
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/persistence/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.imolerodev.pizza.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @Column(length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean locked;
+
+    @Column(nullable = false)
+    private Boolean disabled;
+}

8efdc9d4a47c55c048867fc427534ec6194cd2ff
Ivan Molero <imolerod@gmail.com>
2024-03-10 17:38:13 -0500

    creación del filtro para validación del jwt

---
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.imolerodev.pizza.web.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // validar que sea un header Authorizacion valido
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || authHeader.isEmpty() || authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // validar que el JWT sea valido
+        String jwt = authHeader.split(" ")[1].trim();
+        if (!this.jwtUtil.isValid(jwt)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // cargar el usuario del UserDetailService
+        String username = this.jwtUtil.getUsername(jwt);
+        User user = (User) this.userDetailsService.loadUserByUsername(username);
+
+
+        // cargar al usuario con el contrato de seguridad
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}

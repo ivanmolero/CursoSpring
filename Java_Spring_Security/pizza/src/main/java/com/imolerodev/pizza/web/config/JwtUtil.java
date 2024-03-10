83b9715e5785ac5695493d7c007103b53546ee8d
Ivan Molero <imolerod@gmail.com>
2024-03-10 00:33:41 -0500

    implementado el soporte para JWT con una biblioteca de Auth0, revisar pom.xml, creación de clas JwtUtil para la generación del JWT, revisar JwtUtil

---
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.imolerodev.pizza.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+    private static String SECRET_KEY = "platzi-pizza";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("platy-pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}

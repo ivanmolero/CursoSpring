8488ddd63516af6cd06707a2f4472342360f54f3
Ivan Molero <imolerod@gmail.com>
2024-03-10 20:25:51 -0500

    finalizada la configuración del filtro, con pruebas de uso, se finaliza la implementación de jwt como seguridad para el proyecto

--- Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/JwtFilter.java
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/JwtFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,7 +27,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // validar que sea un header Authorizacion valido
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (authHeader == null || authHeader.isEmpty() || authHeader.startsWith("Bearer")) {
+        if (authHeader == null || authHeader.isEmpty() || !authHeader.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -45,8 +46,9 @@ public class JwtFilter extends OncePerRequestFilter {
 
         // cargar al usuario con el contrato de seguridad
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
-
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        System.out.println(authenticationToken);
         filterChain.doFilter(request, response);
     }
 }
--- Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/SecurityConfig.java
+++ Java_Spring_Security/pizza/src/main/java/com/imolerodev/pizza/web/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.imolerodev.pizza.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,13 +9,19 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,9 +50,11 @@ public class SecurityConfig {
                     .hasRole("ADMIN") // solo se atienden si tienen el rol admin
                     .anyRequest()
                     .authenticated();
-        }).httpBasic(Customizer.withDefaults())
+//        }).httpBasic(Customizer.withDefaults())
+        }).addFilterBefore(jwtFilter, BasicAuthenticationFilter.class)
                 .csrf(csrfConfigurer -> csrfConfigurer.disable())
-                .cors(Customizer.withDefaults());
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
